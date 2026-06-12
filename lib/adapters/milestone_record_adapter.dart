a09999a4783976e306d4518fd24c38f44dc1b824
xiaohope <51153760+xiaohope@users.noreply.github.com>
2026-06-12 08:16:46 +0800

    add lib/adapters/milestone_record_adapter.dart

---
+++ lib/adapters/milestone_record_adapter.dart
@@ -0,0 +1,40 @@
+import 'package:hive/hive.dart';
+import '../models/milestone_record.dart';
+
+part 'milestone_record.g.dart';
+
+@HiveType(typeId: 4)
+class MilestoneRecordBox extends HiveObject {
+  @HiveField(0)
+  late String id;
+
+  @HiveField(1)
+  late DateTime date;
+
+  @HiveField(2)
+  late String title;
+
+  @HiveField(3)
+  String? note;
+
+  @HiveField(4)
+  late String category;
+
+  MilestoneRecordBox.fromModel(MilestoneRecord record) {
+    id = record.id;
+    date = record.date;
+    title = record.title;
+    note = record.note;
+    category = record.category;
+  }
+
+  MilestoneRecord toModel() {
+    return MilestoneRecord(
+      id: id,
+      date: date,
+      title: title,
+      note: note,
+      category: category,
+    );
+  }
+}
