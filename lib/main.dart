ed44b817ca8a9467bc538acd193190e1e1211d83
xiaohope <xiaohope@users.noreply.github.com>
2026-06-12 20:54:00 +0800

    fix: 添加 SystemUiOverlayStyle import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'services/data_service.dart';
