e3ab585a75ebb8e668823b7f60d8623b876f0f38
xiaohope <xiaohope@users.noreply.github.com>
2026-06-14 18:15:36 +0800

    fix: 加回timezone导入

--- lib/screens/reminder_screen.dart
+++ lib/screens/reminder_screen.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz_data;
+import 'package:timezone/timezone.dart' as tz;
 import '../models/reminder_record.dart';
 
 class ReminderScreen extends StatefulWidget {
