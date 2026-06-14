cfbbdc4c7b90b81307878b14b6225f2ea8a94288
xiaohope <xiaohope@users.noreply.github.com>
2026-06-14 14:58:14 +0800

    fix: 缺少provider import

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../services/api_service.dart';
 import '../services/auth_service.dart';
 import '../services/data_service.dart';
